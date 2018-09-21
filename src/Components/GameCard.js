ee10503596d65540ec233292b3f5bbb2106482cd
rpyoungdale <youngdale95@gmail.com>
2018-09-21 15:55:32 -0500

    displays the number of games on a given day without info right now

---
+++ src/Components/GameCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GameCard = () => {
+  return <div>Individual Game</div>;
+};
+
+export default GameCard;
--- src/Containers/GameScores.js
+++ src/Containers/GameScores.js
@@ -1,11 +1,15 @@
 import React from "react";
+import GameCard from "../Components/GameCard";
+
+const baseURL = "http://localhost:3000";
 
 class GameScores extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      currentDate: ""
+      currentDate: "",
+      gameList: []
     };
   }
 
@@ -20,11 +24,41 @@ class GameScores extends React.Component {
     this.setState({
       currentDate: `${year}${month}${day}`
     });
+    this.getScores(`${year}${month}${day}`);
   }
+
+  getScores = date => {
+    // debugger;
+    fetch(`${baseURL}/game-scores`, {
+      method: "POST",
+      body: JSON.stringify({
+        current_date: date
+      }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          gameList: json.games
+        });
+      });
+  };
 
   render() {
     console.log(this.state);
-    return <div style={{ margin: 100 }}>SHOWS TODAYS GAMES</div>;
+    return (
+      <div style={{ margin: 100 }}>
+        <h1>{this.state.gameList.length} Games Today</h1>
+        {this.state.gameList.length
+          ? this.state.gameList.map(game => {
+              return <GameCard key={game.gameId} />;
+            })
+          : null}
+      </div>
+    );
   }
 }
 
